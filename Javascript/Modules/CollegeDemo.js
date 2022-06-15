cc65feddbd0ec896dbf8307a184415c6af458202
kokanevina <kokane.vina@gmail.com>
2022-06-15 17:03:37 +0530

    dd
    dd

--- Javascript/Modules/CollegeDemo.js
+++ Javascript/Modules/CollegeDemo.js
@@ -2,9 +2,15 @@
 exports.__esModule = true;
 var College_1 = require("./MyModule/College");
 var College_2 = require("./MyModule/College");
+var dept = require("./MyModule/Department");
 console.log(College_1.collegName);
 //collegName="LT";   // can not update the values of properties imported from module
 (0, College_1.changeCollege)("LT");
 var newname = (0, College_1.getCollege)();
 console.log(newname);
 console.log(College_2["default"]);
+var dobj = new dept.Deparment();
+dobj.deptId = 5677;
+dobj.deptName = "CSE";
+var libobje = new dept.Library();
+libobje.libraryName = "Mahatma Phule Library";
---
+++ Javascript/Modules/MyModule/Department.js
@@ -0,0 +1,15 @@
+"use strict";
+exports.__esModule = true;
+exports.Library = exports.Deparment = void 0;
+var Deparment = /** @class */ (function () {
+    function Deparment() {
+    }
+    return Deparment;
+}());
+exports.Deparment = Deparment;
+var Library = /** @class */ (function () {
+    function Library() {
+    }
+    return Library;
+}());
+exports.Library = Library;
