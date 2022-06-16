1351ceea9da35491a32fbbeb042223d4da82a256
kokanevina <kokane.vina@gmail.com>
2022-06-16 15:56:37 +0530

    React Components
    App

---
+++ REACT/reactcomponents/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App(){
+  return (
+    <section>
+      <h2 className="Myheading">Sachin Tendulkar</h2>
+      <figure>
+        <img class="MyImage" src="https://www.indiantelevision.com/sites/default/files/styles/smartcrop_800x800/public/images/tv-images/2019/05/30/sachin.jpg?itok=HQ6hzHL8" alt="sachin" ></img>
+        <figcaption>Sachin Tendulkar</figcaption>
+      </figure>
+    </section>
+  );
+}
+
+export default App;
