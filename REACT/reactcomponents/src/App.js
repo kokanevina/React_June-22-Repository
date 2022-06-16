bc004ffa4c2f081518ef21d713247b89e7add513
kokanevina <kokane.vina@gmail.com>
2022-06-16 16:37:15 +0530

    Component
    Info

--- REACT/reactcomponents/src/App.js
+++ REACT/reactcomponents/src/App.js
@@ -1,14 +1,23 @@
+import React from 'react';
 import './App.css';
-
+import {Info} from './InfoComponent/Info';
 function App(){
+  let cricker="Sachin Tendulkarrrrr";
+  let altMessage="sachin";
   return (
-    <section>
-      <h2 className="Myheading">Sachin Tendulkar</h2>
-      <figure>
-        <img class="MyImage" src="https://www.indiantelevision.com/sites/default/files/styles/smartcrop_800x800/public/images/tv-images/2019/05/30/sachin.jpg?itok=HQ6hzHL8" alt="sachin" ></img>
-        <figcaption>Sachin Tendulkar</figcaption>
-      </figure>
-    </section>
+    <React.Fragment>
+      <article>
+      <section>
+        <h2 className="Myheading">{cricker}</h2>
+        <figure>
+          <img className="MyImage" src="https://www.indiantelevision.com/sites/default/files/styles/smartcrop_800x800/public/images/tv-images/2019/05/30/sachin.jpg?itok=HQ6hzHL8" alt={altMessage} />
+          <figcaption>Sachin Tendulkar</figcaption>
+        </figure>
+      </section>
+      <Info></Info>
+      </article>
+
+    </React.Fragment>
   );
 }
 
