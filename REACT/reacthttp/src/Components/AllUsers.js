2979f43c9e853ad65b3dbc2178587337ff9d8a40
kokanevina <kokane.vina@gmail.com>
2022-06-29 17:25:01 +0530

    get request
    asynch await

--- REACT/reacthttp/src/Components/AllUsers.js
+++ REACT/reacthttp/src/Components/AllUsers.js
@@ -1,7 +1,19 @@
-
+import { useEffect,useState } from "react";
+import { getAllUser } from "../Services/api";
 
 
 export function AllUsers(){
+    let [users, setUsers]=useState([]);
+
+    useEffect(()=>{
+        getUsers();
+    },[]);
+
+    let getUsers=async ()=>{
+        let response=   await getAllUser();
+        setUsers(response.data);
+    }
+
     return(
         <table border={3}>
             <thead>
@@ -14,7 +26,15 @@ export function AllUsers(){
                 </tr>
             </thead>
             <tbody>
-
+            {
+                users.map((ele)=>
+                    <tr key={ele.id}>
+                        <td>{ele.id}</td><td>{ele.name}</td>
+                        <td>{ele.username}</td><td>{ele.email}</td>
+                        <td>{ele.phone}</td>
+                    </tr> 
+                )
+            }
             </tbody>
         </table>
     );
---
+++ REACT/reacthttp/src/Services/api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const url="http://127.0.0.1:3001/user"
+
+
+export let getAllUser=async ()=>{
+ return await axios.get(url);        // get request to json server
+}
